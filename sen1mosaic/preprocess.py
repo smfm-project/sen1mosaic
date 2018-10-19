5e2c287d47a68c4b2ca247679e28f56d8e4ce6e7
sambowers <sam.bowers@ed.ac.uk>
2018-10-19 13:13:23 +0100

    Updated for compatability with F-TEP

--- sen1mosaic/preprocess.py
+++ sen1mosaic/preprocess.py
@@ -665,6 +665,8 @@ if __name__ == '__main__':
     
     assert len(infiles) > 0, "No valid input files detected."
     
+    print 'Is f-tep really updating?'
+    
     # Convert arguments to absolute paths    
     infiles = np.array(sorted([os.path.abspath(i) for i in infiles])) # Also sort, and convert to an array.
     output_dir = os.path.abspath(args.output_dir)
